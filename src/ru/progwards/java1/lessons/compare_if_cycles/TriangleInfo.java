58362910837617ee62ec544d0ee8f23f1838a7cb
ilfar <gub.ilfar@yandex.ru>
2020-02-24 17:36:48 +0300

    Progward

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class TriangleInfo {
     public static boolean isTriangle(int a, int b, int c){
-        if (a + b > c || a + c > b || b + c > a)
+        if ((a + b) > c && (a + c) > b && (b + c) > a)
             return true;
         else
             return false;
@@ -23,7 +23,7 @@ public class TriangleInfo {
     }
 
     public static void main(String[] args) {
-        System.out.println("Это треугольник? Ответ: " + isTriangle(3, 4, 5));
+        System.out.println("Это треугольник? Ответ: " + isTriangle(3, 4, 7));
         System.out.println("Это прямоугольный треугольник? Ответ: " + isRightTriangle(3, 4, 4));
         System.out.println("Это равнобедренный треугольник? Ответ: " + isIsoscelesTriangle(3, 3, 3));
     }
