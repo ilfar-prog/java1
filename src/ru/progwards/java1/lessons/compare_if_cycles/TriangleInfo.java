1e804b93814911e2b2ecbdbb8b5c397754f5f16c
ilfar <gub.ilfar@yandex.ru>
2020-02-24 15:32:51 +0300

    Progward

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -8,6 +8,13 @@ public class TriangleInfo {
             return false;
     }
 
+    public static boolean isRightTriangle(int a, int b, int c){
+        if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+            return true;
+            else
+                return false;
+    }
+
     public static boolean isIsoscelesTriangle(int a, int b, int c){
         if (a == b || b == c || a == c)
             return true;
@@ -17,6 +24,7 @@ public class TriangleInfo {
 
     public static void main(String[] args) {
         System.out.println("Это треугольник? Ответ: " + isTriangle(3, 4, 5));
+        System.out.println("Это прямоугольный треугольник? Ответ: " + isRightTriangle(3, 4, 4));
         System.out.println("Это равнобедренный треугольник? Ответ: " + isIsoscelesTriangle(3, 3, 3));
     }
 }
